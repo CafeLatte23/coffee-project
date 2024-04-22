dba867e297579e143f37a84b6529f7c41f69d19f
ChiWei-J <angeljan01@gmail.com>
2024-04-21 21:10:29 -0500

    Updated js script

--- src/index.js
+++ src/index.js
@@ -2,6 +2,34 @@ function chatNow() {
   let name = prompt("What is your name?");
   let age = prompt("How old are you?");
 
+  // Check if inputs are not empty and age is a number
+  if (name && age && !isNaN(age)) {
+    if (age < 18) {
+      alert("Sorry, you may not apply 😭");
+    } else {
+      let email = prompt("What is your email address?");
+      if (email) {
+        alert(
+          "Thanks " +
+            name +
+            ", we've sent you a link to the discussion board! Look forward to seeing you there! 🤩"
+        );
+      } else {
+        alert("Email address is required.");
+      }
+    }
+  } else {
+    alert("Please provide valid inputs.");
+  }
+}
+
+let chatButton = document.querySelector("button");
+chatButton.addEventListener("click", chatNow);
+
+/*function chatNow() {
+  let name = prompt("What is your name?");
+  let age = prompt("How old are you?");
+
   if (age < 18) {
     alert("Sorry, you may not apply 😭");
   } else {
@@ -15,4 +43,4 @@ function chatNow() {
 }
 
 let chatButton = document.querySelector("button");
-chatButton.addEventListener("click", chatNow);
+chatButton.addEventListener("click", chatNow);*/
